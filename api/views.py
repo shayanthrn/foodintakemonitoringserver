4dd05d7d0b904a44d593e92bfd29fdfd30e3aa39
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2023-10-03 17:11:48 -0400

    setup test and file reading

--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
 from django.views import View
 from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+import os
 
+@method_decorator(csrf_exempt, name='dispatch')
 class Analyze(View):
-
-    def get(self,request):
+    def post(self,request):
+        timestamp = int(timezone.now().timestamp())
+        inputfilename = f'./sources/{timestamp}.jpg'
+        os.makedirs(os.path.dirname(inputfilename), exist_ok=True)
+        file = open(inputfilename, 'wb')
+        file.write(request.FILES['pic'].file.read())
         return JsonResponse({'foo':'bar'})
